eb2e14d0ab2b61dd1e0046a1cc41c6b89fcd2a16
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Series_AreaChart/Form1.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace Series_AreaChart {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a new chart.
+            ChartControl areaChart = new ChartControl();
+
+            // Create two area series.
+            Series series1 = new Series("Series 1", ViewType.Area);
+            Series series2 = new Series("Series 2", ViewType.Area);
+
+            // Add points to them.
+            series1.Points.Add(new SeriesPoint(1, 15));
+            series1.Points.Add(new SeriesPoint(2, 18));
+            series1.Points.Add(new SeriesPoint(3, 25));
+            series1.Points.Add(new SeriesPoint(4, 33));
+
+            series2.Points.Add(new SeriesPoint(1, 10));
+            series2.Points.Add(new SeriesPoint(2, 12));
+            series2.Points.Add(new SeriesPoint(3, 14));
+            series2.Points.Add(new SeriesPoint(4, 17));
+
+            // Add both series to the chart.
+            areaChart.Series.AddRange(new Series[] { series1, series2 });
+
+            // Set the numerical argument scale types for the series,
+            // as it is qualitative, by default.
+            series1.ArgumentScaleType = ScaleType.Numerical;
+            series2.ArgumentScaleType = ScaleType.Numerical;
+
+            // Access the view-type-specific options of the series.
+            ((AreaSeriesView)series1.View).Transparency = 80;
+
+            // Access the type-specific options of the diagram.
+            ((XYDiagram)areaChart.Diagram).Rotated = true;
+
+            // Hide the legend (optional).
+            areaChart.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+            // Add the chart to the form.
+            areaChart.Dock = DockStyle.Fill;
+            this.Controls.Add(areaChart);
+        }
+    }
+}
